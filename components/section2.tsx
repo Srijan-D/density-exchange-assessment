171628017df5fd6130afe41844087b7c8e7a2a7b
Srijan Dubey <srijandubey3105@gmail.com>
2023-08-19 20:16:44 +0530

    fix linting errors

--- components/section2.tsx
+++ components/section2.tsx
@@ -1,44 +1,47 @@
-
 import { MotionDiv } from "./motion-div-wrapper"
 
 export function MidSection() {
-    return (
-        <section className='mt-28 flex flex-col gap-8 px-8 text-center font-[Montserrat] md:mt-40 md:flex-row md:gap-6 md:px-40 md:text-start'>
-            <MotionDiv
-                initial={{ y: 20, opacity: 0 }}
-                transition={{
-                    delay: 0.3,
-                    duration: 0.6
-                }}
-                whileInView={{ y: 0, opacity: 1 }}
-                viewport={{ once: true }}
-                className='flex flex-[0.4]  items-start justify-center gap-6 text-xl font-bold text-[#212121] md:text-3xl'>
-                Lorem ipsum dolor sit consectetur.
-                <br />
-                Lorem, ipsum.
-            </MotionDiv>
-            <MotionDiv
-                initial={{ y: 20, opacity: 0 }}
-                transition={{
-                    delay: 0.6,
-                    duration: 0.6
-                }}
-                whileInView={{ y: 0, opacity: 1 }}
-                viewport={{ once: true }}
-                className='mx-5 flex flex-[0.3] items-start justify-center text-lg font-semibold text-[#363636]'>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi voluptas doloribus atque harum velit.
-            </MotionDiv>
-            <MotionDiv
-                initial={{ y: 20, opacity: 0 }}
-                transition={{
-                    delay: 0.8,
-                    duration: 0.6
-                }}
-                whileInView={{ y: 0, opacity: 1 }}
-                viewport={{ once: true }}
-                className='mx-5 flex flex-[0.3] items-start justify-center text-lg font-semibold text-[#363636]'>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            </MotionDiv>
-        </section>
-    )
+  return (
+    <section className="mt-28 flex flex-col gap-8 px-8 text-center md:mt-40 md:flex-row md:gap-6 md:px-40 md:text-start">
+      <MotionDiv
+        initial={{ y: 20, opacity: 0 }}
+        transition={{
+          delay: 0.3,
+          duration: 0.6,
+        }}
+        whileInView={{ y: 0, opacity: 1 }}
+        viewport={{ once: true }}
+        className="flex flex-[0.4]  items-start justify-center gap-6 text-xl font-bold text-[#212121] md:text-3xl"
+      >
+        Lorem ipsum dolor sit consectetur.
+        <br />
+        Lorem, ipsum.
+      </MotionDiv>
+      <MotionDiv
+        initial={{ y: 20, opacity: 0 }}
+        transition={{
+          delay: 0.6,
+          duration: 0.6,
+        }}
+        whileInView={{ y: 0, opacity: 1 }}
+        viewport={{ once: true }}
+        className="mx-5 flex flex-[0.3] items-start justify-center text-lg font-semibold text-[#363636]"
+      >
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi
+        voluptas doloribus atque harum velit.
+      </MotionDiv>
+      <MotionDiv
+        initial={{ y: 20, opacity: 0 }}
+        transition={{
+          delay: 0.8,
+          duration: 0.6,
+        }}
+        whileInView={{ y: 0, opacity: 1 }}
+        viewport={{ once: true }}
+        className="mx-5 flex flex-[0.3] items-start justify-center text-lg font-semibold text-[#363636]"
+      >
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+      </MotionDiv>
+    </section>
+  )
 }
--- components/signature.tsx
+++ components/signature.tsx
@@ -1,44 +1,59 @@
-import Image from 'next/image'
+import Image from "next/image"
+
 import { MotionDiv } from "./motion-div-wrapper"
-import signature from '../public/signature.png'
 
 export function Signature() {
-    return (
-        <section className='mt-40 flex items-center justify-center px-4 font-[Montserrat] md:px-24 '>
-            <MotionDiv
-                initial={{ y: 80, opacity: 0 }}
-                transition={{
-                    delay: 0.4,
-                    duration: 0.8
-                }}
-                whileInView={{ y: 0, opacity: 1 }}
-                viewport={{ once: true }}
-                className='flex flex-col items-center justify-center gap-3 text-center' >
-                <p className='text-sm font-semibold text-[#333333] '>We take privacy seriously</p>
-                <h1 className='text-3xl font-bold text-black'>Before you get started</h1>
-                <p className='text-lg font-semibold text-[#535353]'>"We wont share answers with anyone (and won't ever tell
-                    <br />
-                    you which friends said what about you)"</p>
-                <div className='flex items-center justify-center text-center'>
-
-                    <p className='my-2 inline text-sm font-semibold text-[#363636]'>with love, </p>
-                    <Image src={signature} width={150} height={150} className='ml-2 inline' />
-                </div>
-                <MotionDiv
-                    initial={{ scale: 0, opacity: 0 }}
-                    transition={{
-                        delay: 0.4,
-                        duration: 0.6
-                    }}
-                    whileInView={{ scale: 1, opacity: 1 }}
-                    viewport={{ once: true }}
-                    className='mt-2 flex items-center justify-center rounded-[50px] bg-black px-6 py-3 text-[#f3f3f3] shadow-2xl duration-300 hover:shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]'>
-                    Start a test
-                </MotionDiv>
-                <p className='text-sm font-medium text-[#808080] '>Takes only 5 minutes</p>
-
-            </MotionDiv>
-
-        </section>
-    )
+  return (
+    <section className="mt-40 flex items-center justify-center px-4 font-[Montserrat] md:px-24 ">
+      <MotionDiv
+        initial={{ y: 80, opacity: 0 }}
+        transition={{
+          delay: 0.4,
+          duration: 0.8,
+        }}
+        whileInView={{ y: 0, opacity: 1 }}
+        viewport={{ once: true }}
+        className="flex flex-col items-center justify-center gap-3 text-center"
+      >
+        <p className="text-sm font-semibold text-[#333333] ">
+          We take privacy seriously
+        </p>
+        <h1 className="text-3xl font-bold text-black">
+          Before you get started
+        </h1>
+        <p className="text-lg font-semibold text-[#535353]">
+          &quot;We wont share answers with anyone (and won&apos;t ever tell
+          <br />
+          you which friends said what about you)&quot;
+        </p>
+        <div className="flex items-center justify-center text-center">
+          <p className="my-2 inline text-sm font-semibold text-[#363636]">
+            with love,{" "}
+          </p>
+          <Image
+            src="/signature.svg"
+            alt="signature"
+            width={150}
+            height={150}
+            className="ml-2 inline"
+          />
+        </div>
+        <MotionDiv
+          initial={{ scale: 0, opacity: 0 }}
+          transition={{
+            delay: 0.4,
+            duration: 0.6,
+          }}
+          whileInView={{ scale: 1, opacity: 1 }}
+          viewport={{ once: true }}
+          className="mt-2 flex items-center justify-center rounded-[50px] bg-black px-6 py-3 text-[#f3f3f3] shadow-2xl duration-300 hover:shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]"
+        >
+          Start a test
+        </MotionDiv>
+        <p className="text-sm font-medium text-[#808080] ">
+          Takes only 5 minutes
+        </p>
+      </MotionDiv>
+    </section>
+  )
 }
